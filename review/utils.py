da64954a107dcd5062232db4600c92c0a7ac63bc
ryo <ryo@dytechlab.com>
2022-06-07 12:19:52 +0800

    safely indexing

--- review/utils.py
+++ review/utils.py
@@ -39,10 +39,14 @@ def generate_context(request_object):
     for question in ordered_questions:
         point = 0
         review = []
-        if bucket_extra and len(bucket_extra) > 1:
-            point = bucket_extra[str(question["id"])] if question["typ"] != "title" else 0
-        if reviews and len(reviews) > 1:
-            review = reviews[str(question["id"])] if str(question["id"]) in reviews else []
+        q_id = str(question["id"]) if "id" in question else "0"
+        q_type = question["typ"] if "typ" in question else ""
+
+        if bucket_extra and q_id in bucket_extra and q_type != "title":
+            point = bucket_extra[q_id]
+        if reviews and q_id in reviews:
+            review = reviews[q_id]
+
         ordered_review.append(
             {
                 "point": point,
@@ -55,7 +59,8 @@ def generate_context(request_object):
             "request": {
                 "title": "{}_{}_{}Q{}".format(request_object.reviewee.username,
                                               request_object.reviewer.username,
-                                              quarter[1], quarter[0]),
+                                              quarter[1] if len(quarter) > 1 else '--',
+                                              quarter[0] if len(quarter) > 0 else '--'),
                 "reviewee": request_object.reviewee.username,
                 "reviewer": request_object.reviewer.username,
                 "quarter_and_year": request_object.quarter_and_year,
