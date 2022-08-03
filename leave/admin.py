f9bae74fa788a5b9813bf0c269a2049fb6aea87d
ryo <ryo@dytechlab.com>
2022-08-03 13:04:30 +0800

    autosend email rules

--- leave/admin.py
+++ leave/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.apps import apps
 from .models import (
-    ConfigEntry, ProratedLeave, HolidayLeave
+    ConfigEntry, ProratedLeave, HolidayLeave, AutoSendEmailRule
 )
 from account.models import LeaveRule
 
 app = apps.get_app_config('leave')
 
 # Register your models here.
-excluded_models = [ConfigEntry, ProratedLeave, HolidayLeave]
+excluded_models = [ConfigEntry, ProratedLeave, HolidayLeave, AutoSendEmailRule]
 
 for model_name, model in app.models.items():
     if model in excluded_models:
@@ -23,7 +23,10 @@ class LeaveRuleAdmin(admin.ModelAdmin):
     list_display = ('country','work_pass','leave_type','days','year',)
 class HolidayLeaveAdmin(admin.ModelAdmin):
     list_display = ('user', 'days','year',)
+class AutoSendEmailRuleAdmin(admin.ModelAdmin):
+    list_display = ('name','trigger','interval',)
 
+admin.site.register(AutoSendEmailRule, AutoSendEmailRuleAdmin)
 admin.site.register(HolidayLeave, HolidayLeaveAdmin)
 admin.site.register(LeaveRule, LeaveRuleAdmin)
 admin.site.register(ProratedLeave, ProratedLeaveAdmin)
--- leave/models.py
+++ leave/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 # Create your models here.
 
 class Leave(models.Model):
@@ -24,15 +22,19 @@ class Leave(models.Model):
             models.Index(fields=['status']),
         ]
 
-@receiver(post_save, sender=Leave)
-def post_save_mentorship(sender, instance=None, created=False, **kwargs):
-    try:
-        if not created and instance.user:
-            profile = User.objects.get(username = instance.user).mentorship
-            print(instance.status)
-            print(profile.department)
-    except Exception as e:
-        print(e)
+# @receiver(post_save, sender=Leave)
+# def post_save_mentorship(sender, instance=None, created=False, **kwargs):
+#     try:
+#         if (not created) and instance.user and instance.status=='approved':
+#             departments = ['HR','OP','SA']
+#             profile = User.objects.get(username = instance.user).mentorship
+#             print(instance.status)
+#             print(profile.department)
+#             if profile.department in departments :
+#                 #send mail here
+#                 pass
+#     except Exception as e:
+#         print(e)
 
 class ConfigEntry(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -78,3 +80,36 @@ class HolidayLeave(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class AutoSendEmailRule(models.Model):
+    name = models.CharField(max_length=260, verbose_name="Template name")
+    receiver = models.ManyToManyField(User,  related_name="receivers", related_query_name="receivers", blank=True)
+    TRIGGER_CHOICES = (
+        ("ON_APPROVED", "On Leave Approved"),
+        ("ON_CREATED", "On Leave Created"),
+        ("ON_REJECTED", "On Leave Rejected"),
+        ("ON_LEAVE_DATE", "On Leave Date"),
+    )
+    trigger = models.CharField(max_length=100, blank=True, verbose_name="Select trigger rules") 
+    CC_LIST_CHOICES = (
+        ("HR_TEAM", "HR Team"),
+        ("TEAM_LEAD", "User's Team Lead"),
+        ("TEAM_MATES", "User's Teammates"),
+        ("MENTOR", "User's Mentor"),
+    )
+    cc = models.CharField(max_length=100, blank=True, verbose_name="Select cc list")
+    all_users_on_leave = models.BooleanField(default=False, verbose_name="Get all users list for template")
+    all_users_on_leave = models.TextField(
+        verbose_name="""Email Template\n
+        keyword :\n
+        [type] = type of leave \n
+        [name] = username, [team] = department\n
+        [start] = start_date, [end] = end_date
+        """)
+    INTERVAL_CHOICES = (
+        ("EVERY_MORNING", "Every 8 am SGT"),
+        ("ON_TRIGGER", "On Trigger"),
+    )
+    interval = models.CharField(max_length=20,
+                    choices=INTERVAL_CHOICES,
+                    default="ON_TRIGGER")
