7c35f928251047bca6025fa326616ae4218d96e9
ryo <ryo@dytechlab.com>
2022-08-11 10:06:18 +0800

    merged with master

--- account/models.py
+++ account/models.py
@@ -6,11 +6,10 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from auditlog.registry import auditlog
 from . import signals
+from datetime import datetime, timedelta
 
-from leave.utils import (update_work_pass, update_children_amount,
-                         update_singapore_holiday, update_join_date,
-                         update_personal_childcare_leave,
-                         save_personal_extra, get_extra)
+from leave.models import ConfigEntry, ProratedLeave, HolidayLeave
+import json
 # Create your models here.
 
 class LowerCaseField(models.CharField):
@@ -117,6 +116,62 @@ class LeaveRule(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+def get_extra(instance):
+    extra = None
+    ce = ConfigEntry.objects.filter(name="leave_type_{}".format(datetime.now().year)).first()
+    if ce :
+        extra = json.loads(ce.extra)
+
+    pr = ProratedLeave.objects.filter(name="{}_leave_{}".format(instance.user.username, datetime.now().year)).first()
+    if pr :
+        extra = json.loads(pr.extra)
+    return extra
+
+def save_personal_extra(extra, instance):
+    if not extra:
+        return
+    pr = ProratedLeave.objects.filter(name="{}_leave_{}".format(instance.user.username, datetime.now().year)).first()
+    if not pr:
+        ProratedLeave.objects.create(
+            user=instance.user,
+            name="{}_leave_{}".format(instance.user.username, datetime.now().year),
+            extra=json.dumps(extra)
+        )
+    else :
+        pr.extra = json.dumps(extra, indent=2)
+        pr.save()
+
+def update_personal_childcare_leave(profile):
+    rule = LeaveRule.objects.filter(
+                                    country=profile.country,
+                                    work_pass = profile.work_pass,
+                                    leave_type = "CC",
+                                    year = datetime.now().year
+                                    ).first()
+    if not rule:
+        rule = LeaveRule.objects.filter(
+                                country=profile.country,
+                                work_pass = profile.work_pass,
+                                leave_type = "CC"
+                                ).order_by("-updated_at").first()
+    days = 0 
+    if rule and profile.children_birth_year:
+        eligible_children = 0
+        for birth_year in profile.children_birth_year.split(";"):
+            if int(birth_year) + rule.children_max_age >= datetime.now().year:
+                eligible_children += 1
+        if eligible_children:
+            days = eligible_children * rule.days
+            days = days if days <= rule.max_days else rule.max_days
+
+    extra = get_extra(profile)
+
+    pro_rated_leaves = []
+    for leave in extra:
+        if leave["name"] == "childcare":
+            leave["limitation"] = days
+        pro_rated_leaves.append(leave)
+    save_personal_extra(pro_rated_leaves, profile)
 
 @receiver(post_save, sender=LeaveRule)
 def update_country_leave_rules(sender, instance, **kwargs):
@@ -140,6 +195,58 @@ def update_country_leave_rules(sender, instance, **kwargs):
                 pro_rated_leaves.append(leave)
             save_personal_extra(pro_rated_leaves, user)
 
+def update_singapore_holiday(instance=None):
+    additional = 0
+    if instance.country.country_code == "SG":
+        hl = HolidayLeave.objects.get_or_create(user=instance.user, year=datetime.now().year)[0]
+        sample = HolidayLeave.objects.filter(year = datetime.now().year).first()
+        if sample:
+            hl.days = sample.days
+            hl.extra = sample.extra
+            hl.save()
+            additional = sample.days
+    return additional
+
+def update_children_amount(instance=None):
+    if instance.children_birth_year:
+        try:
+            instance.children = len(instance.children_birth_year.split(";"))
+            if len(instance.children_birth_year) > 0 and instance.children == 0:
+                instance.children = 1
+        except:
+            pass
+    else:
+       instance.children = 0 
+
+def update_work_pass(instance=None):
+    singapore = Country.objects.get_or_create(name="Singapore", country_code="SG")[0]
+    if instance.work_pass and instance.country != singapore:
+        instance.work_pass = None
+
+def update_join_date(instance=None):
+    extra = None
+    ce = ConfigEntry.objects.filter(name="leave_type_{}".format(datetime.now().year)).first()
+    if ce :
+        extra = json.loads(ce.extra)
+    if instance.join_date and extra:
+        this_start_year = datetime.fromisoformat("{}-01-01 00:00:00".format(datetime.now().year))
+        this_end_year = datetime.fromisoformat("{}-12-31 00:00:00".format(datetime.now().year))
+        this_year_days = (this_end_year - this_start_year).days
+        join_with_time = datetime(
+            year=instance.join_date.year, 
+            month=instance.join_date.month,
+            day=instance.join_date.day,
+        )
+        delta = this_end_year - join_with_time
+        pro_rated_leaves = []
+        for leave in extra:
+            if leave["name"] == "personal":
+                holiday = update_singapore_holiday(instance)
+                additional = (delta.days/this_year_days) * (leave["limitation"] + holiday)
+                final = round(additional*2) / 2 - holiday
+                leave["limitation"] = final if final < leave["limitation"] else leave["limitation"]
+            pro_rated_leaves.append(leave)
+        save_personal_extra(pro_rated_leaves, instance)
 
 @receiver(pre_save, sender=Mentorship)
 def presave_mentorship(sender, instance=None, created=False, **kwargs):
--- okr_app/migrations/0003_auto_20211214_2146.py
+++ okr_app/migrations/0003_auto_20211214_2146.py
@@ -14,7 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='okrfile',
             name='okr',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
-                                    related_name='files', to='okr_app.OKR'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='files', to='okr_app.OKR'),
         ),
     ]
